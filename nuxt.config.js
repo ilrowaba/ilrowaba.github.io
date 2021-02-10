79420e1efb4c456764a2e5d7320fbbdb570bfcf0
김도영 <95doyoung@gmail.com>
2021-02-10 20:57:49 +0900

    CONFIG BUILD OPTIONS

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,7 +65,10 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    publicPath:
+      process.env.NODE_ENV === 'production' ? '/ilrowaba.github.io/' : '/',
+  },
   generate: {
     dir: 'docs',
   },
